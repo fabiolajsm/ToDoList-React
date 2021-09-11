e2febb61b17e1d93c10780b2855d3cfc22efc50f
Fabiola Suárez <81818021+fabiolajsm@users.noreply.github.com>
2021-09-11 20:24:38 -0300

    input addTodo styles

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 import { useDispatch } from 'react-redux';
 import { addTodo } from '../../redux/actions';
 
@@ -19,7 +20,14 @@ export default function AddTodo() {
   return (
     <div>
       <form className="form-inline" onSubmit={handleSubmit}>
-        <input className="form-control mr-sm-2" type="text" placeholder="Write your task..." value={task} onChange={handleChange} />
+        <TextField
+          label="Write your task"
+          placeholder="..."
+          variant="outlined"
+          value={task}
+          onChange={handleChange}
+        />
+        {/* <input className="form-control mr-sm-2" type="text" placeholder="Write your task..." value={task} onChange={handleChange} /> */}
         <button className="btn btn-outline-secondary my-2 my-sm-0" onClick={handleSubmit}>Add</button>
       </form>
     </div>
